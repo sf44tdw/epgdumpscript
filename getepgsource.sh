1508ef7f860ea8ae72b13827e4bd5c75eb10ec03
sf44tdw <prmkaddr@yahoo.co.jp>
2014-10-03 01:33:42 +0900

    20141003
    無料放送にチャンネルを合わせられない。
    チャンネル番号を間違えたのか、単に受信設備がないのか、ともかくCS番組表の取得が今後の課題。

--- getepgsource.sh
+++ getepgsource.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
-#EPG取得のため、地上波物理チャンネル全てと、BS/CSチャンネルから1つずつを選択し、90秒間録画する。
-#BSch=101(NHK BS1) CSch=100(スカパー！プロモ100)
+#EPG取得のため、地上波物理チャンネル全てと、BSチャンネルから1つずつを選択し、90秒間録画する。
+#BSch=101(NHK BS1)
 
 #カレントディレクトリ
 cdir=`dirname $0`
-echo ${cdir}
 
-#tsファイル保存先
+#tsファイル保存先ディレクトリ
 tsdir=${cdir}/ts
 
-rm ${tsdir}/*.ts
+#EPG XMLファイル保存先ディレクトリ
+epgdir=${cdir}/epg_xml
 
-for channel in 21 22 23 24 25 26 27 28 100 101
+#放送局種別
+btype=0
+
+#前回のファイルが残っているかも知れないので、念のため削除
+rm -f ${tsdir}/*.ts
+rm -f ${epgdir}/*.xml
+
+#in以降にチャンネル番号をスペースで区切って記入する。
+for channel in 21 22 23 24 25 26 27 28 101
 
 do
 
+echo ${channel}
+
 /usr/local/bin/recpt1 --strip --b25 ${channel} 90 ${tsdir}/${channel}.ts
 
+case ${channel} in
+
+#BS放送
+ 101 )
+ btype="BS";;
+
+#地上波
+ *   )
+ btype=${channel};;
+
+esac
+
+echo ${btype}
+epgdump ${btype} ${tsdir}/${channel}.ts ${epgdir}/${channel}.xml
+
 done
+
+rm -f ${tsdir}/*.ts
