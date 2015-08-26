8664e6f990a6d99879d962b9ffae9b0fc53afdf6
sf44tdw <prmkaddr@yahoo.co.jp>
2015-08-27 06:21:16 +0900

    指定時間外でも強制終了

--- getEPG.sh
+++ getEPG.sh
@@ -4,16 +4,23 @@
 #BSch=101(NHK BS1)
 
 #スクリプトのログディレクトリ
-LogDir=${HOME}/Log/epgDumpLog
+LogDir=${HOME}/Log
+
+LogDir_epgDump=${LogDir_P}/epgDumpLog
 if [ ! -e ${LogDir} ]; then
 `mkdir ${LogDir}`
+fi
+
+LogDir_epgDump=${LogDir_P}/epgDumpLog
+if [ ! -e ${LogDir_epgDump} ]; then
+`mkdir ${LogDir_epgDump}`
 fi
 
 #日付取得
 Date=`date "+%Y%m%d%H%M%S"`
 #ファイル名生成
 FileName="D"${Date}"P"$$
-LogFile=${LogDir}"/"${FileName}".log"
+LogFile=${LogDir_epgDump}"/"${FileName}".log"
 
 echo ${LogFile} > ${LogFile}
 
@@ -23,20 +30,50 @@ _lockfile="/tmp/`basename $0`.lock"
 ln -s /dummy $_lockfile 2> /dev/null || { echo 'Cannot run multiple instance.' >> ${LogFile}; exit 9; }
 trap "rm $_lockfile; exit" 1 2 3 15
 
+
+NowHour=`date +%H`
+if [ ! "06" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "12" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "16" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "18" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "22" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "24" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "00" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+if [ ! "02" -eq ${NowHour} ]; then  
+  echo "Hour is "${NowHour} >> ${LogFile}
+  exit 1
+fi
+
 # ファイル更新日時が10日を越えたログファイルを削除
 PARAM_DATE_NUM=10
-find ${LogDir} -name "*.log" -type f -mtime +${PARAM_DATE_NUM} -exec rm -f {} \;
+find ${LogDir_epgDump} -name "*.log" -type f -mtime +${PARAM_DATE_NUM} -exec rm -f {} \;
 
 #ワークディレクトリをこのスクリプトが置かれている場所にする。
 cd `dirname $0`
 
 pdir=${HOME}
 
-#tsファイル保存先ディレクトリ
-tsdir=${pdir}/ts
-if [ ! -e ${tsdir} ]; then
-`mkdir ${tsdir}`
-fi
 
 #EPG XMLファイル保存先ディレクトリ
 epgdir=${pdir}/epg_xml
@@ -56,10 +93,10 @@ for channel in 21 22 23 24 25 26 27 28 101
 #for channel in 21 22
 
 do
-echo "*******************************************************************************" >> ${LogFile}
-echo ${channel} >> ${LogFile}
+echo "*******************************************************************************" >> ${LogDir_epgDump}
+echo ${channel} >> ${LogDir_epgDump}
 
-/usr/local/bin/recpt1 --strip --b25 ${channel} 90 ${tsdir}/${channel}.ts 1>>${LogFile} 2>&1
+/usr/local/bin/recpt1 --strip --b25 ${channel} 90 ${tsdir}/${channel}.ts 1>>${LogDir_epgDump} 2>&1
 
 case ${channel} in
 
@@ -75,9 +112,30 @@ esac
 
 #echo ${btype}
 /usr/local/bin/epgdump ${btype} ${tsdir}/${channel}.ts ${epgdir}/${channel}.xml
-echo "*******************************************************************************" >> ${LogFile}
+echo "*******************************************************************************" >> ${LogDir_epgDump}
 done
 
 rm -f ${tsdir}/*.ts
+
+LogDir_UpdateDB=${LogDir_P}/epgDumpLog/epgUpdaterLog
+if [ ! -e ${LogDir_UpdateDB} ]; then
+`mkdir ${LogDir_epgDump}`
+fi
+
+#EPGDB更新を行う。
+
+#EPGDB更新プログラムとDTDファイルのディレクトリ(更新プログラムの設定ファイルはここに置く)
+DBUpdaterDir=${HOME}/EPGUpdater
+
+#EPGDB更新プログラム(jar)のパス
+DBUpdater=${DBUpdaterDir}/EPGUpdater.jar
+
+#EPG XMLファイル保存先ディレクトリ
+epgdir=${pdir}/epg_xml
+
+java -jar ${DBUpdater} ${DBUpdaterDir} "UTF-8" ${epgdir}
+
+rm -f ${epgdir}/*.xml
+
 
 rm $_lockfile
